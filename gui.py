f8ebbfb80918e6d21cbca2442e312d6a4a31938d
Enrique Cametti <quiquecametti97@gmail.com>
2021-09-02 21:57:31 -0300

    stdout on button click

--- gui.py
+++ gui.py
@@ -1,38 +1,81 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout, QLineEdit
-from PyQt5.QtGui import QPixmap
-from PyQt5 import QtGui, QtCore
-from PyQt5.QtGui import QCursor
- 
-app = QApplication(sys.argv)
-qss = "styles.qss"
-with open(qss,"r") as fh:
-    app.setStyleSheet(fh.read())
+from PyQt5.QtCore import Qt, QTimer, pyqtSignal
+from PyQt5.QtWidgets import *
+# from PyQt5.QtGui import QPixmap, QCursor
+# from PyQt5 import QtGui, QtCore
 
-window = QWidget()
-window.setWindowTitle("WhatsApp Android Caller")
-# window.setFixedWidth(1000)
-# window.setStyleSheet("background: #202020;")
 
-grid = QGridLayout()
-window.setLayout(grid)
+class WhatsAppCaller(QDialog):
+    def __init__(self, parent = None):
+        super(WhatsAppCaller, self).__init__(parent)
+        self.setMinimumSize(320,140)
+        self.originalPalette = QApplication.palette()
+        qss = "styles.qss"
+        with open(qss,"r") as fh:
+            self.setStyleSheet(fh.read())
+        self.createMainWindow()
+    
+    def createMainWindow(self):
+        # Contact textbox
+        self.contactTB = QLineEdit(self)
+        self.contactTB.setPlaceholderText("Insert contact info (name or phone)")
+        self.contactTB.setAlignment(Qt.AlignCenter)
+        # Call button
+        pushButton = QPushButton("Call contact")
+        pushButton.clicked.connect(self.buttonClicked)
+        # Place widgets in window
+        layout = QGridLayout()
+        layout.addWidget(self.contactTB)
+        layout.addWidget(pushButton)
+        self.setLayout(layout)
 
-# Logo
-# img = QPixmap("img/whatsapp-bot-icon.png")
-# logo = QLabel()
-# logo.setObjectName("WhatsAppLogo")
-# logo.setPixmap(img)
-# logo.setAlignment(QtCore.Qt.AlignCenter)
-# grid.addWidget(logo)
+    def buttonClicked(self):
+        print("Button was pushed")
 
-# Contact text box
-contacttb = QLineEdit("Insert contact info")
-grid.addWidget(contacttb)
+if __name__ == '__main__':
 
-# Push button
-button = QPushButton("Call contact")
-button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-grid.addWidget(button)
+    import sys
 
-window.show()
-sys.exit(app.exec())
+    app = QApplication(sys.argv)
+    caller = WhatsAppCaller()
+    caller.show()
+    sys.exit(app.exec_()) 
+
+
+
+
+
+
+
+# app = QApplication(sys.argv)
+# qss = "styles.qss"
+# with open(qss,"r") as fh:
+#     app.setStyleSheet(fh.read())
+
+# window = QWidget()
+# window.setWindowTitle("WhatsApp Android Caller")
+# # window.setFixedWidth(1000)
+# # window.setStyleSheet("background: #202020;")
+
+# grid = QGridLayout()
+# window.setLayout(grid)
+
+# # Logo
+# # img = QPixmap("img/whatsapp-bot-icon.png")
+# # logo = QLabel()
+# # logo.setObjectName("WhatsAppLogo")
+# # logo.setPixmap(img)
+# # logo.setAlignment(QtCore.Qt.AlignCenter)
+# # grid.addWidget(logo)
+
+# # Contact text box
+# contacttb = QLineEdit("Insert contact info")
+# grid.addWidget(contacttb)
+
+# # Push button
+# button = QPushButton("Call contact")
+# button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+# grid.addWidget(button)
+
+# window.show()
+# sys.exit(app.exec())
